ba67c71de0aba4d1e20c24beadbd30f744214415
ABisman1 <asherb40@nycstudents.net>
2026-06-01 14:09:15 -0400

    Create FinalProject.py

---
+++ FinalProject.py
@@ -0,0 +1,17 @@
+from tkinter import *
+#need to install on all machines
+from tkmacosx import Button
+import os
+import tkinter.messagebox as box
+
+root = Tk()
+root.title("Home")
+
+#Set size of window
+root.geometry("2000x1000")
+
+
+
+
+
+root.mainloop()
